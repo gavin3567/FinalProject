91aa1d38165c9989d1343fb662aed7dc6471067e
bgriffff <brandonjosephgriffiths@gmail.com>
2022-10-29 09:09:12 -0600

    about page

--- app_exercise/urls.py
+++ app_exercise/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, aboutPageView
 urlpatterns = [
-    path("",indexPageView,name="index")
+    path("",indexPageView,name="index"),
+    path("about/", aboutPageView, name="about"),
 ]
