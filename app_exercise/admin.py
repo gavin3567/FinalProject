690a91a191b79a003f92b7916b66379757de3cf4
pwarner3 <plouwarner@gmail.com>
2022-11-16 16:53:50 -0700

    working models

--- app_exercise/admin.py
+++ app_exercise/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Person, Workout, Wokout_Group
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Workout)
+admin.site.register(Wokout_Group)
--- app_exercise/models.py
+++ app_exercise/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Wokout_Group(models.Model):
+    category_name = models.CharField(max_length=20)
+
+    def __str__(self) :
+        return (self.category_name)
+    
+    class Meta:
+        db_table = 'workout_cat'
+
+class Workout(models.Model):
+    workout_name = models.CharField(max_length=50)
+    category_id = models.ForeignKey(Wokout_Group, on_delete = models.CASCADE)
+    # main_photo = models.ImageField(upload_to='photos')          
+    # leave_date = models.DateField(default=datetime.today, blank=True)
+
+    def __str__ (self):
+        return (self.workout_name)
+    
+    class Meta:
+        db_table = 'workout'
+    
+    
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)    
+    phone = models.CharField(max_length=13, blank=True)
+    workout = models.ManyToManyField(Workout, blank=True)
+
+    def __str__(self):
+        return (self.full_name)
+    
+    @property
+    def full_name(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    #override the save method
+    def save(self):
+        self.first_name = self.first_name.upper()
+        self.last_name = self.last_name.upper()
+        super(Person, self).save() # Call the "real" save() method.              
+
+    class Meta:
+        db_table = 'person'
