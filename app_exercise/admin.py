b034b345aba51d3ba89521e8457eac7bc81e16b3
pwarner3 <plouwarner@gmail.com>
2022-11-17 00:26:42 -0700

    admin table additions

--- app_exercise/admin.py
+++ app_exercise/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Person, Workout, Wokout_Group
+from .models import Person, Workout, Wokout_Group, Person_Workout, Person_Workout_Data
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Workout)
 admin.site.register(Wokout_Group)
+admin.site.register(Person_Workout)
+admin.site.register(Person_Workout_Data)
+
