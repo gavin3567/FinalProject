fce4e24a675b9f186fadc752d8688a079a564605
pwarner3 <plouwarner@gmail.com>
2022-11-17 00:11:50 -0700

    main model changes

--- app_exercise/admin.py
+++ app_exercise/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Person, Workout, Wokout_Group
+from .models import Person, Workout, Wokout_Group, Person_Workout_Data, Person_Workout
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Workout)
 admin.site.register(Wokout_Group)
+admin.site.register(Person_Workout)
+admin.site.register(Person_Workout_Data)
