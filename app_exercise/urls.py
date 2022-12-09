3290bc61b607a43aeb55a191949ec6989ea4e0d6
bgriffff <brandonjosephgriffiths@gmail.com>
2022-12-08 20:55:22 -0700

    new dashboard

--- app_exercise/urls.py
+++ app_exercise/urls.py
@@ -3,10 +3,12 @@ from .views import indexPageView, loginPageView
 from .views import addingPageView
 from .views import listPageView
 from .views import aboutPageView
+from .views import dashboardPageView
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("login/", loginPageView, name="login"),
     path("adding/", addingPageView, name ="adding"),
     path("listing/", listPageView, name ="listing"),
     path("about/", aboutPageView, name ="about"),
+    path("dashboard/", dashboardPageView, name ="dashboard"),
 ]
