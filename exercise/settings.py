ef8a7fd41249a9e5cb8cd4f036c0e72bf2a55b90
gavin3567 <gavinostheimer00@gmail.com>
2022-12-12 13:52:37 -0700

    this2

--- exercise/settings.py
+++ exercise/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://web-production-a93f.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c26f.up.railway.app']
 
 
 # Application definition
