85a1143f61bca140c4089f5522669437357e1b32
gavin3567 <gavinostheimer00@gmail.com>
2022-12-12 11:49:09 -0700

    Add database

--- exercise/settings.py
+++ exercise/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,19 +75,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'exercise.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-            'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'exercise_project',
-            'USER': 'admin_user',
-            'PASSWORD': 'admin123',
-            'HOST': 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+
+# # Database
+# # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+# DATABASES = {
+#             'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'exercise_project',
+#             'USER': 'admin_user',
+#             'PASSWORD': 'admin123',
+#             'HOST': 'localhost'
+#     }
+# }
 
 
 # Password validation
